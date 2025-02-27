a71d9fe8f053122a619896ae9d1cc258f1949b5c
LukeMcCann <developer-lukemccann@outlook.com>
2025-02-27 21:05:05 +0000

    Fix configuration issues with storybook

--- packages/nimbus-core/.storybook/main.ts
+++ packages/nimbus-core/.storybook/main.ts
@@ -4,6 +4,15 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
   framework: '@storybook/react-vite',
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
 };
 
 export default config;
